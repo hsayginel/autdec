c502490e72a2072a0664ae915b217316577ec013
StergiosKou <167451653+StergiosKou@users.noreply.github.com>
2025-03-02 13:21:56 +0000

    add random choice of automorphisms

--- autdec/igraph_auts.py
+++ autdec/igraph_auts.py
@@ -98,6 +98,23 @@ def list_perm_group_elements_schreier(group,n):
     return elts
 
 def graph_auts_from_bliss(pcm,print_order=True):
+    """
+    Computes the automorphism group of a Tanner graph derived from a parity-check matrix (pcm) using the BLISS algorithm.
+
+    This function takes a parity-check matrix, constructs the corresponding Tanner graph,
+    and then calculates the automorphism group of this graph. It returns a list of
+    automorphism permutations on the graph vertices. 
+
+    Args:
+        pcm (numpy.ndarray or scipy.sparse.csr_matrix): The parity-check matrix.
+        print_order (bool, optional): If True, prints the order of the automorphism group.
+                                      Defaults to True.
+
+    Returns:
+        auts_list: A list of graph automorphism permutations, where each permutation
+                               is represented as a NumPy array. The length of each permutation
+                               is the sum of the number of check nodes and variable nodes.
+    """
     m,n=pcm.shape
     tanner_graph=sparse_pcm_to_tanner_graph(pcm)
     autgroup=graph_aut_group(tanner_graph, print_order=print_order)
@@ -108,6 +125,27 @@ def graph_auts_from_bliss(pcm,print_order=True):
     return auts_list
 
 def vertex_graph_auts_from_bliss(pcm,print_order=True):
+    """
+    Computes the vertex-wise automorphism group of a Tanner graph derived from a parity-check matrix (PCM) 
+    using the BLISS algorithm.
+
+    This function takes a parity-check matrix, constructs the corresponding Tanner graph,
+    and then calculates the automorphism group of this graph. It returns separate lists of
+    permutation matrices representing the automorphisms acting on the column (variable) nodes
+    and row (check) nodes of the Tanner graph. If we print the order of the automorphism group,
+    sympy runs Schreier Sims on the background, so it's faster to enumerate elements with the same method.
+    Otherwise, Dimino's enumeration method is faster for relatively small groups.
+
+    Args:
+        pcm (numpy.ndarray or scipy.sparse.csr_matrix): The parity-check matrix.
+        print_order (bool, optional): If True, prints the order of the automorphism group.
+                                      Defaults to True.
+
+    Returns:
+        tuple (list of numpy.ndarray, list of numpy.ndarray): A tuple containing two lists:
+            - The first list contains permutation matrices representing automorphisms on the column nodes.
+            - The second list contains permutation matrices representing automorphisms on the row nodes.
+    """
     m,n=pcm.shape
     tanner_graph=sparse_pcm_to_tanner_graph(pcm)
     autgroup=graph_aut_group(tanner_graph, print_order=print_order)
@@ -129,4 +167,68 @@ def vertex_graph_auts_from_bliss(pcm,print_order=True):
                 row_perm = PX[n:,n:]
                 row_perms.append(row_perm)
                 col_perms.append(col_perm)
+    return col_perms, row_perms
+
+def random_permutation_group_elements(grp, k):
+    """
+    Generates k distinct random elements from a SymPy permutation group, excluding the identity.
+
+    Args:
+        grp (sympy.combinatorics.perm_groups.PermutationGroup): The permutation group.
+        k (int): The number of distinct random elements to generate.
+
+    Returns:
+        list of sympy.combinatorics.permutations.Permutation: A list of k distinct random permutations.
+    """
+    # if k > group.order() - 1:
+    #     raise ValueError("k cannot be greater than the order of the group minus 1 (excluding identity).")
+
+    identity = Permutation(grp.degree)
+    elements = set()
+    while len(elements) < k:
+        if grp.schreier_sims:
+            random_element = grp.random()
+        else:
+            random_element = grp.random_element_dimino()
+        if random_element != identity:
+            elements.add(random_element)
+    return list(elements)
+
+
+def random_vertex_graph_auts_from_bliss(pcm, k, print_order=True):
+    """
+    Computes the vertex-wise automorphism group of a Tanner graph derived from a parity-check matrix (PCM) 
+    using the BLISS algorithm and returns k random elements from it.
+
+    This function takes a parity-check matrix, constructs the corresponding Tanner graph,
+    and then calculates the automorphism group of this graph. It returns separate lists of
+    permutation matrices representing the automorphisms acting on the column (variable) nodes
+    and row (check) nodes of the Tanner graph. If we print the order of the automorphism group,
+    sympy runs Schreier Sims on the background, so it's faster to enumerate elements with the same method.
+    Otherwise, Dimino's enumeration method is faster for relatively small groups.
+
+    Args:
+        pcm: The parity-check matrix.
+        k: Number of random permutations to take.
+        print_order (optional): If True, prints the order of the automorphism group.
+                                Defaults to True.
+
+    Returns:
+        tuple (list of numpy.ndarray, list of numpy.ndarray): A tuple containing two lists:
+            - The first list contains permutation matrices representing automorphisms on the column nodes.
+            - The second list contains permutation matrices representing automorphisms on the row nodes.
+    """
+    m,n=pcm.shape
+    tanner_graph=sparse_pcm_to_tanner_graph(pcm)
+    autgroup=graph_aut_group(tanner_graph, print_order=print_order)
+    rand_elements=random_permutation_group_elements(autgroup,k)
+    row_perms = []
+    col_perms = [] 
+    for element in rand_elements:
+        if element!=Permutation(range(n+m)):
+            PX=permutation_matrix(element,n+m)
+            col_perm = PX[:n,:n]
+            row_perm = PX[n:,n:]
+            row_perms.append(row_perm)
+            col_perms.append(col_perm)
     return col_perms, row_perms
